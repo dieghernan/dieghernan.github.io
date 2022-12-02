1cfbf08071097a46869f056640211e66d375d600
dieghernan <dieghernan@users.noreply.github.com>
2022-12-02 14:59:16 +0000

    jekyll build from Action 2e46f9f5dd43619b4b3e64a3980f8e56c3daee12

--- assets/js/google/google-search.js
+++ assets/js/google/google-search.js
@@ -1,7 +1,6 @@
 (function() {
     var cx = '';
     var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
     gcse.async = true;
     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
